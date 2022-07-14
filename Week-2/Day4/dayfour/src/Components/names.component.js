cc8ec267425ab331f17b4e5388e6c026d1f38028
saniya58 <sohelshk1858@gmail.com>
2022-07-14 16:33:24 +0530

    add: myname

--- Week-2/Day4/dayfour/src/Components/names.component.js
+++ Week-2/Day4/dayfour/src/Components/names.component.js
@@ -7,6 +7,7 @@ const Names = () => {
              <li class="list-group-item">Janhavi</li>
              <li class="list-group-item">SANVED</li>
              <li class="list-group-item">PRAJAKTA</li>
+             <li class="list-group-item">Saniya</li>
   
   
         </ul>
