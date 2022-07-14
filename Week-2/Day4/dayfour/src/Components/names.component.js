32802f6ef7866f947dd4a4cc5c9042165e87794a
prajakta-deu <deoguneprajakta@gmail.com>
2022-07-14 16:14:40 +0530

    add:name

--- Week-2/Day4/dayfour/src/Components/names.component.js
+++ Week-2/Day4/dayfour/src/Components/names.component.js
@@ -6,6 +6,7 @@ const Names = () => {
              <li class="list-group-item">Shivam</li>
              <li class="list-group-item">Janhavi</li>
              <li class="list-group-item">SANVED</li>
+             <li class="list-group-item">PRAJAKTA</li>
   
   
         </ul>
