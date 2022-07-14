eb6827aa89573dff1ce817bb5cd3b81d35514d34
supriyaphatangre <supriyaphatangre7@gmail.com>
2022-07-14 16:13:58 +0530

    new file

--- Week-2/Day4/dayfour/src/Components/names.component.js
+++ Week-2/Day4/dayfour/src/Components/names.component.js
@@ -5,7 +5,7 @@ const Names = () => {
           <ul class="list-group">
              <li class="list-group-item">Shivam</li>
 
-             <li class="list-group-item">Supriya</li>
+ 
 
              <li class="list-group-item">Janhavi</li>
 
